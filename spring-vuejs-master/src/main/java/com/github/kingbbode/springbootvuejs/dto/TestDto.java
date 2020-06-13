3872ac51cf8ee4ed51c085c8cbe0c078868bb292
kdy41000 <kdy41000@naver.com>
2020-06-13 16:40:39 +0900

    Dto추가

--- spring-vuejs-master/src/main/java/com/github/kingbbode/springbootvuejs/dto/TestDto.java
+++ spring-vuejs-master/src/main/java/com/github/kingbbode/springbootvuejs/dto/TestDto.java
@@ -1,4 +1,14 @@
 package com.github.kingbbode.springbootvuejs.dto;
 
 public class TestDto {
+
+    private String testOne;
+
+    public String getTestOne() {
+        return testOne;
+    }
+
+    public void setTestOne(String testOne) {
+        this.testOne = testOne;
+    }
 }
