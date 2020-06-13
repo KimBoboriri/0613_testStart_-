2fb65b0ba3c72d60b378940de2fa9ed20dea32b4
kdy41000 <kdy41000@naver.com>
2020-06-13 17:17:06 +0900

    앞단 바인딩 추가

--- spring-vuejs-master/src/main/java/com/github/kingbbode/springbootvuejs/controller/DomiNewController.java
+++ spring-vuejs-master/src/main/java/com/github/kingbbode/springbootvuejs/controller/DomiNewController.java
@@ -4,4 +4,15 @@ public class DomiNewController {
     //도미가 변경했다
     //domi는 지웠다
     //요기 작업하세요 김도미씨앗
+
+    /*
+
+    * 담당자 : 김동영
+    * 일시:05:16
+    * 앞단 바인딩작업 추가
+    */
+    public String TestDomiNew(){
+        int binding = 0;
+        return "";
+    }
 }
