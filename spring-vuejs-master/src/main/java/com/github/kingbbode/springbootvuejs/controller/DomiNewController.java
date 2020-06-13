851b8524d4135864d6bc1e60ec1e0d9e95c52e5a
takorina12 <takorina12@naver.com>
2020-06-13 17:11:31 +0900

    김도미 씨앗님 DomiNewController 생성하였씁니다

---
+++ spring-vuejs-master/src/main/java/com/github/kingbbode/springbootvuejs/controller/DomiNewController.java
@@ -0,0 +1,7 @@
+package com.github.kingbbode.springbootvuejs.controller;
+
+public class DomiNewController {
+    //도미가 변경했다
+    //domi는 지웠다
+    //요기 작업하세요 김도미씨앗
+}
--- spring-vuejs-master/src/main/java/com/github/kingbbode/springbootvuejs/controller/TestboboController.java
+++ spring-vuejs-master/src/main/java/com/github/kingbbode/springbootvuejs/controller/TestboboController.java
@@ -2,6 +2,4 @@ package com.github.kingbbode.springbootvuejs.controller;
 
 public class TestboboController {
     //보보의 테스트르 위한컨트롤러
-    //도미가 변경했다
-    //domi는 지웠다
 }
